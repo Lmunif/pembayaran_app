97faf35e9310b90161a74310e12890797ceaf0df
alwani achmad <alwaniachmad7031999aa@gmail.com>
2026-02-15 18:36:55 +0700

    chore: update localhost IP address for iOS simulator connectivity
    Use 192.168.7.248 instead of 127.0.0.1 to allow iOS simulator to connect to the local backend server when running on physical device network

--- pembayaran_app/lib/services/auth_service.dart
+++ pembayaran_app/lib/services/auth_service.dart
@@ -19,7 +19,7 @@ class AuthService {
   // Use Android emulator host mapping, otherwise localhost
   final String baseUrl = Platform.isAndroid
       ? 'http://10.0.2.2:8080/api'
-      : 'http://127.0.0.1:8080/api';
+      : 'http://192.168.7.248:8080/api';
 
   Future<LoginResponse> login(String username, String password) async {
     try {
